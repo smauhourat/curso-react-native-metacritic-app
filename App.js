519bdaf6a81525d5d592643a6fdf79e408af2ccd
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2025-10-24 17:51:06 -0300

    main

--- App.js
+++ App.js
@@ -1,43 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import { useEffect, useState } from "react";
-import { getProducts } from "./lib/api";
-
-import icon from "./assets/icon.png";
+import { Main } from "./components/Main";
 
 export default function App() {
-  const [productos, setProductos] = useState([]);
-
-  useEffect(() => {
-    async function fetchProductos() {
-      const response = await getProducts();
-      setProductos(response);
-    }
-    fetchProductos();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{ width: 128, height: 128 }} />
-      <Text>Productos disponibles:</Text>
-      <ScrollView>
-        {productos.data &&
-          productos.data.map((producto) => (
-            <>
-              <Image
-                source={{
-                  uri: `https://picsum.photos/200/300?random=${producto.id}`,
-                }}
-                style={{ width: 50, height: 50, borderRadius: 10 }}
-              />
-              <Text key={producto.id}>
-                {producto.id} - {producto.nombre} - ${producto.precio_promedio}
-              </Text>
-            </>
-          ))}
-      </ScrollView>
       <StatusBar style="auto" />
+      <Main />
     </View>
   );
 }
@@ -45,14 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff" /*#1976d2*/,
-    alignItems: "center",
+    backgroundColor: "#ddd",
+    // backgroundColor: "#fff"
+    /*#1976d2*/ alignItems: "center",
     justifyContent: "center",
-  },
-  btnPpal: {
-    backgroundColor: "#1976d2",
-    padding: 10,
-    borderRadius: 10,
-    marginTop: 10,
   },
 });
