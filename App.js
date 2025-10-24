481a30248781e41278d0b5223b2ec1bb9cacf129
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2025-10-24 18:50:57 -0300

    css3

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { Main } from "./components/Main";
+import { Logo } from "./components/Logo";
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      <Logo />
       <View style={styles.container}>
         <StatusBar style="auto" />
         <Main />
